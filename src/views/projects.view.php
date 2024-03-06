723eb776f003edb091c942ccd04e7d99fb77665b
Harrymullabu <hmullabu@gmail.com>
2024-03-06 17:07:24 -0600

    Update projects.view.php

--- src/views/projects.view.php
+++ src/views/projects.view.php
@@ -7,13 +7,13 @@ $project = new ProjectModel($projectData);
 
     <article>
 
-        <a href = <?php echo $project->get_link()?>> <img src= <?php echo $URL_BASE . $project->get_img()?> alt=""> </a> 
+        
 
         <div>
         <h2><?php echo $project->get_title()?></h2>
         <p><?php echo $project->get_desc()?></p>
         <p><?php echo "Languages: " . $project->get_lang()?></p>
-
         </div>
+        <a href = <?php echo $project->get_link()?>>Github</a>
 
     </article>
